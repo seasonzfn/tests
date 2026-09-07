4944efa89b44b354e9dcc5ee888d5fc44e6c71bb
Benedict <benedicttega2008@gmail.com>
2026-09-08 01:21:33 +0200

    button interupt

--- main.c
+++ main.c
@@ -1,40 +1,75 @@
-
 #include "pico/stdlib.h"
 #include "stdio.h"
 
-#define LED 1
-#define BUTTON 16
+#define BUTTON1 16
+#define BUTTON2 17
 
-volatile bool led_on = false;
+uint8_t mask = 0x7F;
 
-// one general gpio callback function
+
+uint8_t value = (1 << 0) | (1 << 1);
+int direction = 1;
+
+
 void gpio_callback(uint gpio, uint32_t events) {
-	if (gpio == BUTTON && (events & GPIO_IRQ_EDGE_FALL)) {
-		gpio_xor_mask( (1<<LED) );
-	} 
-	// else if (GPIO == OTHERBUTTON) && (events & GPIO_IRQ_EDGE_FALL)) {
-	// do_something_else();
-	// } 
+
+    if (gpio == BUTTON1 && (events & GPIO_IRQ_EDGE_FALL)) {
+
+        gpio_put_masked(mask, value);
+        sleep_ms(100);
+
+        if (direction == 1) {
+            value <<= 1;
+        } else {
+            value >>= 1;
+        }
+
+
+        if (value == ((1 << 6) | (1 << 7))) {
+            direction = -1;
+        }
+        if (value == ((1 << 0) | (1 << 1))) {
+            direction = 1;
+        }
+    }
+
+    if (gpio == BUTTON2 && (events & GPIO_IRQ_EDGE_RISE)) {
+        gpio_put_masked(mask, 0);
+        value = (1 << 0) | (1 << 1);
+        direction = 1;
+    }
 }
 
 int main() {
-    stdio_init_all(); // needed for picotool to auto load
+    stdio_init_all();
 
-	// init IO
-	gpio_init_mask( (1<<LED) | (1 <<BUTTON) );
- 	gpio_set_dir_all_bits( (1<<LED) | (0 <<BUTTON) );
-	gpio_pull_up( BUTTON );
-	gpio_xor_mask( (1<<LED) );
-		
-	// register interrupt callback
-	gpio_set_irq_enabled_with_callback(
-		BUTTON,
-		GPIO_IRQ_EDGE_FALL,
-		true,
-		&gpio_callback
-	);
-	
-	while (true) {
-		tight_loop_contents(); // Idle loop
-	}
+
+    gpio_init_mask(mask);
+    gpio_set_dir_masked(mask, mask);
+
+    gpio_init(BUTTON1);
+    gpio_init(BUTTON2);
+
+    gpio_set_dir(BUTTON1, GPIO_IN);
+    gpio_set_dir(BUTTON2, GPIO_IN);
+
+    gpio_pull_up(BUTTON1);
+    gpio_pull_up(BUTTON2);
+
+    gpio_set_irq_enabled_with_callback(
+        BUTTON1,
+        GPIO_IRQ_EDGE_RISE,
+        true,
+        &gpio_callback
+    );
+
+    gpio_set_irq_enabled(
+        BUTTON2,
+        GPIO_IRQ_EDGE_FALL,
+        true
+    );
+
+    while (true) {
+        tight_loop_contents();
+    }
 }
