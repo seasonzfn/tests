e397c285713affc7a2d42030878f3dac9cdc2b8c
Benedict <benedicttega2008@gmail.com>
2026-09-08 01:59:26 +0200

    button interupt

--- main.c
+++ main.c
@@ -3,44 +3,19 @@
 
 #define BUTTON1 16
 #define BUTTON2 17
-#define DEBOUNCE_US 20000
 
 uint8_t mask = 0x7F;
 uint8_t value = (1 << 0) | (1 << 1);
 int direction = 1;
-
-volatile uint32_t last_irq1 = 0;
-volatile uint32_t last_irq2 = 0;
+volatile int scanning = 0;
 
 void gpio_callback(uint gpio, uint32_t events) {
-    uint32_t now = time_us_32();
-
     if (gpio == BUTTON1 && (events & GPIO_IRQ_EDGE_FALL)) {
-        while(!BUTTON2){
-            if (now - last_irq1 < DEBOUNCE_US) return;
-            last_irq1 = now;
-    
-            gpio_put_masked(mask, value);   // no sleep_ms here
-    
-            if (direction == 1) {
-                value <<= 1;
-            } else {
-                value >>= 1;
-            }
-    
-            if (value == ((1 << 5) | (1 << 6))) {
-                direction = -1;
-            }
-            if (value == ((1 << 0) | (1 << 1))) {
-                direction = 1;
-            }
-        }
+        scanning = 1;
     }
 
     if (gpio == BUTTON2 && (events & GPIO_IRQ_EDGE_FALL)) {
-        if (now - last_irq2 < DEBOUNCE_US) return;
-        last_irq2 = now;
-
+        scanning = 0;
         gpio_put_masked(mask, 0);
         value = (1 << 0) | (1 << 1);
         direction = 1;
@@ -60,7 +35,7 @@ int main() {
     gpio_set_dir(BUTTON2, GPIO_IN);
 
     gpio_pull_up(BUTTON1);
-    gpio_pull_down(BUTTON2);
+    gpio_pull_up(BUTTON2);
 
     gpio_set_irq_enabled_with_callback(
         BUTTON1,
@@ -76,6 +51,17 @@ int main() {
     );
 
     while (true) {
+        if (scanning == 1) {
+            gpio_put_masked(mask, value);
+            sleep_ms(100);
+
+            if (direction == 1) value <<= 1;
+            else value >>= 1;
+
+            if (value == ((1 << 5) | (1 << 6))) direction = -1;
+            if (value == ((1 << 0) | (1 << 1))) direction = 1;
+        }
+
         tight_loop_contents();
     }
 }
